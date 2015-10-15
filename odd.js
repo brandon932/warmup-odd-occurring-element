2f88ae8b5ca0a926d09dba0601020c5c65d96fa7
brandon932 <bthornton_932@yahoo.com>
2015-10-15 14:35:20 -0600

    refactored to accept strings for values in array as well

--- odd.js
+++ odd.js
@@ -1,16 +1,17 @@
 testArr = [1,2,3,1,2,3,2,2,2,2,2,2,3];
 myArr = [1,1,2,3,3];
+testArr2= ["a","a","b","c","c","b","c"];
 
 
 var findOdd = function(arr){
   workArr = arr.sort();
   console.log(workArr);
   for (var i = 0; i < workArr.length; i += 2) {
-    if ((workArr[i] + workArr[i+1]) % 2 !== 0){
+    if (workArr[i] !==  workArr[i+1]){
       console.log(workArr[i]);
       return workArr[i];
     }
   }
 };
 
-findOdd(testArr);
+findOdd(testArr2);
